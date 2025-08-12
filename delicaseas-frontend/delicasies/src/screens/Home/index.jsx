1c5c5711c41c0bfc250a82bd4acb32feabb3400c
faseh <khan.faseh@gmail.com>
2025-08-12 12:49:15 +0500

    more changes to signup page

--- delicaseas-frontend/delicasies/src/screens/Home/index.jsx
+++ delicaseas-frontend/delicasies/src/screens/Home/index.jsx
@@ -1,64 +1,55 @@
 import React from 'react';
 import styles from './styles.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={`container-fluid ${styles.page}`}>
+      <div className="row g-0 h-100">
 
-      {/* Left Column - Signup */}
-    <div className={styles.left}>
-        
-      {/* Logo Section */}
-      <div className={styles.logo}>
-          <h1>Delicacies</h1>
-      </div>
+        {/* Left Column */}
+        <div className={`col-12 col-md-6 d-flex justify-content-center align-items-center ${styles.left}`}>
+          <div className={styles.formWrapper}>
+            <div className={styles.logo}>
+              <h1>Delicacies</h1>
+            </div>
 
-      {/* Heading */}
-      <div className={styles.heading}>
-          <h1>Get Started</h1>
-      </div>
+            <h1 className={styles.heading}>Get Started</h1>
 
-       
+            <form className={styles.signupForm}>
+              <label htmlFor="name">Name</label>
+              <input type="text" placeholder="Name" />
 
-         
+              <label htmlFor="email">Email</label>
+              <input type="email" placeholder="Email" />
 
-        {/* Signup Form */}
-       
-        <form className={styles.signupForm}>
-         <label htmlFor="name">Name</label>
-         <input type="text" placeholder="Name" />
+              <label htmlFor="password">Password</label>
+              <input type="password" placeholder="Password" />
 
-         <label htmlFor="email">Email</label>
-         <input type="email" placeholder="Email" />
+              <div className={styles.checkbox}>
+                <input type="checkbox" id="terms" />
+                <label htmlFor="terms">I agree to the terms & conditions</label>
+              </div>
 
-         <label htmlFor="password">Password</label>
-         <input type="password" placeholder="Password" />
+              <button className={styles.signupButton} type="submit">Sign Up</button>
 
-         <div className={styles.checkbox}>
-           <input type="checkbox" id="terms" />
-           <label htmlFor="terms">I agree to the terms & conditions</label>
-         </div>
-         <div className={styles.signupButton}>
-           <button type="submit">Sign Up</button>
-         </div>
+              <div className={styles.accountButtons}>
+                <button type="button">Sign in with Google</button>
+                <button type="button">Sign in with Apple</button>
+              </div>
 
-         <div className={styles.accountButtons}>
-           <button type="submit">Sign in with Google</button>
-           <button type="submit">Sign in with Apple</button>
-         </div>
-
-          <div className={styles.loginLink}>
-            <p>Already have an account? <a href="/login">Login</a></p>
+              <div className={styles.loginLink}>
+                <p>Already have an account? <a href="/login">Login</a></p>
+              </div>
+            </form>
           </div>
-</form>
+        </div>
 
+        {/* Right Column */}
+        <div className={`col-12 col-md-6 ${styles.right}`}>
+          <img src="/images/signup.jpg" alt="Signup visual" />
+        </div>
 
-        
-      </div>
-
-      {/* Right Column - Image */}
-      <div className={styles.right}>
-        <img src="/images/signup.jpg" alt="Signup visual" />
       </div>
     </div>
   );
